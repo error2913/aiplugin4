8364024d812f825b8863f669e54cf0cba86a56cd
error2913 <2913949387@qq.com>
2025-04-12 22:36:04 +0800

    fix: 修复http依赖内部错误时发不出消息的问题

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -36,37 +36,44 @@ export async function replyToSender(ctx: seal.MsgContext, msg: seal.Message, ai:
             return '';
         }
 
-        const messageArray = parseText(s);
+        try {
+            const messageArray = parseText(s);
 
-        const epId = ctx.endPoint.userId;
-        const group_id = ctx.group.groupId.replace(/\D+/g, '');
-        const user_id = ctx.player.userId.replace(/\D+/g, '');
-        if (msg.messageType === 'private') {
-            const data = {
-                user_id,
-                message: messageArray
-            }
-            const result = await globalThis.http.getData(epId, 'send_private_msg', data);
-            if (result?.message_id) {
-                logger.info(`(${result.message_id})发送给QQ:${user_id}:${s}`);
-                return transformMsgId(result.message_id);
+            const epId = ctx.endPoint.userId;
+            const group_id = ctx.group.groupId.replace(/\D+/g, '');
+            const user_id = ctx.player.userId.replace(/\D+/g, '');
+            if (msg.messageType === 'private') {
+                const data = {
+                    user_id,
+                    message: messageArray
+                }
+                const result = await globalThis.http.getData(epId, 'send_private_msg', data);
+                if (result?.message_id) {
+                    logger.info(`(${result.message_id})发送给QQ:${user_id}:${s}`);
+                    return transformMsgId(result.message_id);
+                } else {
+                    throw new Error(`获取消息ID失败`);
+                }
+            } else if (msg.messageType === 'group') {
+                const data = {
+                    group_id,
+                    message: messageArray
+                }
+                const result = await globalThis.http.getData(epId, 'send_group_msg', data);
+                if (result?.message_id) {
+                    logger.info(`(${result.message_id})发送给QQ-Group:${group_id}:${s}`);
+                    return transformMsgId(result.message_id);
+                } else {
+                    throw new Error(`获取消息ID失败`);
+                }
             } else {
-                throw new Error(`在replyToSender中: 获取消息ID失败`);
+                throw new Error(`未知的消息类型`);
             }
-        } else if (msg.messageType === 'group') {
-            const data = {
-                group_id,
-                message: messageArray
-            }
-            const result = await globalThis.http.getData(epId, 'send_group_msg', data);
-            if (result?.message_id) {
-                logger.info(`(${result.message_id})发送给QQ-Group:${group_id}:${s}`);
-                return transformMsgId(result.message_id);
-            } else {
-                throw new Error(`在replyToSender中: 获取消息ID失败`);
-            }
-        } else {
-            throw new Error(`在replyToSender中: 未知的消息类型`);
+        } catch (error) {
+            logger.error(`在replyToSender中: ${error}`);
+            ai.context.lastReply = s;
+            seal.replyToSender(ctx, msg, s);
+            return '';
         }
     } else {
         ai.context.lastReply = s;
