81e4a23bc1aa00bd9ab5059042f3104b30bd2266
baiyu-yu <135424680+baiyu-yu@users.noreply.github.com>
2025-04-25 22:15:19 +0800

    修改后端服务器地址

--- src/config/config_backend.ts
+++ src/config/config_backend.ts
@@ -7,10 +7,10 @@ export class BackendConfig {
         BackendConfig.ext = ConfigManager.getExt('aiplugin4_6:后端');
 
         seal.ext.registerStringConfig(BackendConfig.ext, "流式输出", "http://localhost:3010", '自行搭建或使用他人提供的后端');
-        seal.ext.registerStringConfig(BackendConfig.ext, "图片转base64", "http://162.14.109.222:46678", '可自行搭建');
-        seal.ext.registerStringConfig(BackendConfig.ext, "联网搜索", "http://162.14.109.222:8080", '可自行搭建');
-        seal.ext.registerStringConfig(BackendConfig.ext, "网页读取", "http://162.14.109.222:46799", '可自行搭建');
-        seal.ext.registerStringConfig(BackendConfig.ext, "用量图表", "http://42.193.236.17:3009", '可自行搭建');
+        seal.ext.registerStringConfig(BackendConfig.ext, "图片转base64", "https://urltobase64.白鱼.chat", '可自行搭建');
+        seal.ext.registerStringConfig(BackendConfig.ext, "联网搜索", "https://searxng.白鱼.chat", '可自行搭建');
+        seal.ext.registerStringConfig(BackendConfig.ext, "网页读取", "https://webread.白鱼.chat", '可自行搭建');
+        seal.ext.registerStringConfig(BackendConfig.ext, "用量图表", "http://error.白鱼.chat:3009", '可自行搭建');
     }
 
     static get() {
