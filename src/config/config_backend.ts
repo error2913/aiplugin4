b3d7e418dda6b7e012361a607ffb02a8879238e6
error2913 <2913949387@qq.com>
2025-08-25 22:32:28 +0800

    Update config_backend.ts

--- src/config/config_backend.ts
+++ src/config/config_backend.ts
@@ -7,7 +7,7 @@ export class BackendConfig {
         BackendConfig.ext = ConfigManager.getExt('aiplugin4_6:后端');
 
         seal.ext.registerStringConfig(BackendConfig.ext, "流式输出", "http://localhost:3010", '自行搭建或使用他人提供的后端');
-        seal.ext.registerStringConfig(BackendConfig.ext, "图片转base64", "https://urltobase64.白鱼.chat", '可自行搭建');
+        seal.ext.registerStringConfig(BackendConfig.ext, "图片转base64", "https://urltobase64.fishwhite.top", '可自行搭建');
         seal.ext.registerStringConfig(BackendConfig.ext, "联网搜索", "https://searxng.fishwhite.top", '可自行搭建');
         seal.ext.registerStringConfig(BackendConfig.ext, "网页读取", "https://webread.fishwhite.top", '可自行搭建');
         seal.ext.registerStringConfig(BackendConfig.ext, "用量图表", "http://localhost:3009", '可自行搭建');
