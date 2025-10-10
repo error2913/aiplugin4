b9b3490d7531e5e291374616a614f4fb4e2dcc3f
error2913 <2913949387@qq.com>
2025-10-10 19:55:25 +0800

    Update tool_meme.ts

--- src/tool/tool_meme.ts
+++ src/tool/tool_meme.ts
@@ -1,5 +1,6 @@
 import { Image, ImageManager } from "../AI/image";
 import { ConfigManager } from "../config/config";
+import { logger } from "../logger";
 import { Tool, ToolInfo, ToolManager } from "./tool";
 
 const baseurl = "http://meme.lovesealdice.online/";
@@ -40,7 +41,6 @@ export function registerMeme() {
             }
         }
     }
-
     const tool_list = new Tool(list_info); // 创建一个新tool
     tool_list.solve = async (_, __, ___, ____) => { // 实现方法，返回字符串提供给AI
         try {
@@ -52,11 +52,40 @@ export function registerMeme() {
         }
     }
 
+    const get_info: ToolInfo = {
+        type: "function",
+        function: {
+            name: "get_meme_info",
+            description: `获取表情包制作信息`,
+            parameters: {
+                type: "object",
+                properties: {
+                    name: {
+                        type: "string",
+                        description: "表情包名字,为 meme_list 返回的结果"
+                    }
+                },
+                required: ["name"] // 必需参数
+            }
+        }
+    }
+    const tool_get = new Tool(get_info); // 创建一个新tool
+    tool_get.solve = async (_, __, ___, args) => { // 实现方法，返回字符串提供给AI
+        const { name } = args;
+
+        const { info } = await getInfo(name);
+        const { max_images, max_texts, min_images, min_texts } = info.params_type;
+        const image_text = min_images === max_images ? `用户数量为 ${min_images} 名` : `用户数量范围为 ${min_images} - ${max_images} 名`;
+        const text_text = min_texts === max_texts ? `文字数量为 ${min_texts} 段` : `文字数量范围为 ${min_texts} - ${max_texts} 段`;
+
+        return `该表情包需要：${image_text}，${text_text}`;
+    }
+
     const generator_info: ToolInfo = {
         type: "function",
         function: {
             name: "meme_generator",
-            description: `制作表情包,使用之前需要调用 meme_list获取可用表情包列表`,
+            description: `制作表情包,使用之前需要调用meme_list获取可用表情包列表,调用get_meme_info获取制作信息`,
             parameters: {
                 type: "object",
                 properties: {
@@ -74,22 +103,25 @@ export function registerMeme() {
                         items: { type: "string" },
                         description: '被用来绘制meme的用户名称' + (ConfigManager.message.showNumber ? '或纯数字QQ号' : '')
                     },
+                    save: {
+                        type: "boolean",
+                        description: "是否保存图片"
+                    }
                 },
-                required: ["name", "text", "members"] // 必需参数
+                required: ["name", "text", "members", "save"] // 必需参数
             }
         }
     }
-
     const tool_generator = new Tool(generator_info); // 创建一个新tool
     tool_generator.solve = async (ctx, msg, ai, args) => { // 实现方法，返回字符串提供给AI
-        const { name, text = [], members = [] } = args;
+        const { name, text = [], members = [], save } = args;
 
         let s = '';
 
         const { key, info } = await getInfo(name);
         const { max_images, max_texts, min_images, min_texts } = info.params_type;
-        const image_text = min_images === max_images ? `用户数量为 ${min_images} 名` : `用户范围为 ${min_images} - ${max_images} 名`;
-        const text_text = min_texts === max_texts ? `文字数量为 ${min_texts} 段` : `文字范围为 ${min_texts} - ${max_texts} 段`;
+        const image_text = min_images === max_images ? `用户数量为 ${min_images} 名` : `用户数量范围为 ${min_images} - ${max_images} 名`;
+        const text_text = min_texts === max_texts ? `文字数量为 ${min_texts} 段` : `文字数量范围为 ${min_texts} - ${max_texts} 段`;
         if (text.length > max_texts || text.length < min_texts) {
             if (max_texts === 0) {
                 text.length = 0;
@@ -130,20 +162,30 @@ export function registerMeme() {
             const json = await res.json();
             if (json.status == "success") {
                 const base64 = json.message;
+                if (!base64) {
+                    logger.error(`生成的base64为空`);
+                    return "生成的base64为空";
+                }
+
                 const file = seal.base64ToImage(base64);
-                const newImage = new Image(file);
-                newImage.id = ImageManager.generateImageId(ai, name);
-                newImage.isUrl = false;
-                newImage.scenes = [...text, ...members];
-                newImage.base64 = base64;
-                newImage.content = `表情包${name}
+
+                let id = '';
+                if (save) {
+                    const newImage = new Image(file);
+                    newImage.id = ImageManager.generateImageId(ai, name);
+                    newImage.isUrl = false;
+                    newImage.scenes = [...text, ...members];
+                    newImage.base64 = base64;
+                    newImage.content = `表情包${name}
 文字${text.join('，') || '无'}
 用户${members.join('，') || '无'}`;
 
-                ai.imageManager.updateSavedImages([newImage]);
+                    ai.imageManager.updateSavedImages([newImage]);
+                    id = newImage.id;
+                }
 
                 seal.replyToSender(ctx, msg, `[CQ:image,file=${file}]`)
-                return `${s}发送成功，已保存为<|img:${newImage.id}|>`;
+                return `${s}发送成功${save ? `，已保存为<|img:${id}|>` : ''}`;
             } else {
                 throw new Error(json.message);
             }
@@ -154,5 +196,6 @@ export function registerMeme() {
 
     // 注册到toolMap中
     ToolManager.toolMap[list_info.function.name] = tool_list;
+    ToolManager.toolMap[get_info.function.name] = tool_get;
     ToolManager.toolMap[generator_info.function.name] = tool_generator;
 }
