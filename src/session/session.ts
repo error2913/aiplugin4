a5cc44f74f6f796ac3aeeeb57e3a9304d4c5d9b3
error2913 <2913949387@qq.com>
2026-03-04 21:20:18 +0800

    啥东东这是

--- src/session/session.ts
+++ src/session/session.ts
@@ -1,11 +1,43 @@
+import { GroupInfo } from "../agent/AI";
+import { Context } from "./context";
+import { User } from "./user";
+
+export class State{
+    isPrivate: boolean;
+    group: {
+        groupId: string;
+        groupName: string;
+        owner: string;
+        adminList: string[];
+        memberList: string[];
+        description: string; // 自定义描述
+        impression: string; // ai可修改的印象
+    }
+    user: {
+        userId: string;
+        userName: string;
+        description: string; // 自定义描述
+        impression: string; // ai可修改的印象
+    }
+    tool: {}
+    [key: string]: any;
+}
+
 export class Session {
-    sid: string;
-    state: any;
+    sessionId: string;
+    state: { //储存状态信息
+        groupId: string;
+        userIdArray: string[];
+        [key: string]: any;
+    }
+    context: Context;
     memory: Memory;
+    image;
 }
 
 export class SessionService {
     sessionMap: { [key: string]: Session };
+    userMap: { [key: string]: User };
 
     constructor() {
 
---
+++ src/session/user.ts
@@ -0,0 +1,5 @@
+export class User {
+    uid: string;
+    state: any; //储存状态信息
+    memory: Memory;
+}
