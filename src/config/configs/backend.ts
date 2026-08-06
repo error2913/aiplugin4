4b5de5bcea778d338dc4ca06432f6dfdcfb681e5
error2913 <2913949387@qq.com>
2026-08-07 01:39:19 +0800

    feat: 后端默认配置改为本地回环地址+端口

--- src/config/configs/backend.ts
+++ src/config/configs/backend.ts
@@ -3,11 +3,11 @@ import { ext } from "../config";
 export default class BackendConfig {
     static register() {
         seal.ext.registerStringConfig(ext, "流式输出", "http://localhost:3010", '自行搭建或使用他人提供的后端', "后端");
-        seal.ext.registerStringConfig(ext, "图片转base64", "https://urltobase64.fishwhite.top", '可自行搭建', "后端");
+        seal.ext.registerStringConfig(ext, "图片转base64", "http://127.0.0.1:46678", '可自行搭建', "后端");
         seal.ext.registerStringConfig(ext, "联网搜索", "https://searxng.fishwhite.top", '可自行搭建', "后端");
-        seal.ext.registerStringConfig(ext, "网页读取", "https://webread.fishwhite.top", '可自行搭建', "后端");
-        seal.ext.registerStringConfig(ext, "用量图表", "http://usagechart.error2913.com", '可自行搭建', "后端");
-        seal.ext.registerStringConfig(ext, "md和html图片渲染", "https://md.fishwhite.top", '可自行搭建', "后端");
+        seal.ext.registerStringConfig(ext, "网页读取", "http://127.0.0.1:46799", '可自行搭建', "后端");
+        seal.ext.registerStringConfig(ext, "用量图表", "http://127.0.0.1:3009", '可自行搭建', "后端");
+        seal.ext.registerStringConfig(ext, "md和html图片渲染", "http://127.0.0.1:37632", '可自行搭建', "后端");
         seal.ext.registerStringConfig(ext, "论坛地址", "https://aiplugin-forum.fishwhite.top", 'aiplugin4专用论坛地址', "后端");
         seal.ext.registerStringConfig(ext, "论坛API Token", "", '论坛注册后获取的api_token，用于发帖等写操作的鉴权', "后端");
         seal.ext.registerStringConfig(ext, "论坛签名密钥", "", '论坛注册后获取的secret_key，用于请求签名', "后端");
